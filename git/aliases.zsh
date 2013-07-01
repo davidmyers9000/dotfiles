472e2a66b875bbc755af462d830cc78e563975f2
David Myers <davidmyers9000@gmail.com>
2013-07-01 14:58:11 -0400

    unaliased hub

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,4 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
-fi
+alias git=$BOXEN_HOME/homebrew/bin/git
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
