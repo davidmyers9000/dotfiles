2d0e94704804c5dad55017c1c89068cf70cbeefb
David Myers <davidmyers9000@gmail.com>
2015-07-09 12:19:15 -0400

    updated sublim and added a few aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,4 @@
 alias reload!='. ~/.zshrc'
+alias terminate3000='lsof -P | grep ":3000" | awk "{print $2}" | xargs kill -9'
+alias terminate4000='lsof -P | grep ":4000" | awk "{print $2}" | xargs kill -9'
+alias terminate4567='lsof -P | grep ":4567" | awk "{print $2}" | xargs kill -9'
